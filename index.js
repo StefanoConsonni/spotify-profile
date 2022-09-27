c0a8fb49df8e0288acec90c9fb188d38abf7ac64
Stefano Consonni <stefanoconsonni@ymail.com>
2022-09-27 19:44:57 +0200

    setup dotenv

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+require("dotenv").config();
 const express = require("express");
-
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
 	res.send("hello!");
 });
 
-const port = 3000;
 app.listen(port, () => {
 	console.log(`Serving on port ${port}!`);
 });
