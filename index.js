60f6e0a5bee5096365527dbd34ffed5f782e49b2
Stefano Consonni <stefanoconsonni@ymail.com>
2022-09-27 20:49:20 +0200

    initial routes setup

--- index.js
+++ index.js
@@ -3,8 +3,53 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
 app.get("/", (req, res) => {
-	res.send("hello!");
+	const data = {
+		name: "Mario",
+		isAwesome: true,
+	};
+	res.json(data);
+});
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = (length) => {
+	let text = "";
+	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (let i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+};
+
+const stateKey = "spotify_auth_state";
+
+app.get("/login", (req, res) => {
+	const state = generateRandomString(16);
+	res.cookie(stateKey, state);
+
+	const scope = "user-read-private user-read-email";
+
+	const queryParams = querystring.stringify({
+		client_id: CLIENT_ID,
+		response_type: "code",
+		redirect_uri: REDIRECT_URI,
+		state: state,
+		scope: scope,
+	});
+
+	res.redirect(`https://accounts.spotify.com/authorize?${queryParams}}`);
+});
+
+app.get("callback", (req, res) => {
+	res.send("callback");
 });
 
 app.listen(port, () => {
