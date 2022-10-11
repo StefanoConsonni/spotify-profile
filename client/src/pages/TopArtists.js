bb2e60072663e8117105262011de641f3ced5bb2
Stefano Consonni <stefanoconsonni@ymail.com>
2022-10-11 17:53:29 +0200

    edit Playlist, TopArtists and TopTracks components

--- client/src/pages/TopArtists.js
+++ client/src/pages/TopArtists.js
@@ -1,35 +1,30 @@
-import { useState, useEffect } from 'react';
-import { getTopArtists } from '../spotify';
-import { catchErrors } from '../utils';
-import { ArtistsGrid, SectionWrapper, TimeRangeButtons } from '../components';
+import { useState, useEffect } from "react";
+import { getTopArtists } from "../spotify";
+import { catchErrors } from "../utils";
+import { ArtistsGrid, SectionWrapper, TimeRangeButtons, Loader } from "../components";
 
 const TopArtists = () => {
-  const [topArtists, setTopArtists] = useState(null);
-  const [activeRange, setActiveRange] = useState('short');
+	const [topArtists, setTopArtists] = useState(null);
+	const [activeRange, setActiveRange] = useState("short");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data } = await getTopArtists(`${activeRange}_term`);
-      setTopArtists(data);
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			const { data } = await getTopArtists(`${activeRange}_term`);
+			setTopArtists(data);
+		};
 
-    catchErrors(fetchData());
-  }, [activeRange]);
+		catchErrors(fetchData());
+	}, [activeRange]);
 
-  return (
-    <main>
-      <SectionWrapper title="Top Artists" breadcrumb={true}>
-        <TimeRangeButtons
-          activeRange={activeRange}
-          setActiveRange={setActiveRange}
-        />
+	return (
+		<main>
+			<SectionWrapper title="Top Artists" breadcrumb={true}>
+				<TimeRangeButtons activeRange={activeRange} setActiveRange={setActiveRange} />
 
-        {topArtists && topArtists.items && (
-          <ArtistsGrid artists={topArtists.items} />
-        )}
-      </SectionWrapper>
-    </main>
-  );
+				{topArtists && topArtists.items ? <ArtistsGrid artists={topArtists.items} /> : <Loader />}
+			</SectionWrapper>
+		</main>
+	);
 };
 
 export default TopArtists;
